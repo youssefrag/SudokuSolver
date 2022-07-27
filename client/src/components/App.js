fc3f97501852777d4698b89e7de05f1d3134cc2c
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-27 11:12:31 -0400

    initiate board component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,19 @@
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    paddingTop: '100px',
+    paddingBottom: '500px',
+    backgroundColor: '#D3D3D3',
+  },
+});
+
 function App() {
+
+  const classes= useStyles();
+
   return (
-    <div className="App">
+    <div className={classes.root}>
     </div>
   );
 }
---
+++ client/src/components/ui/Board.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Button, TextField } from '@mui/material';
+
+export default function Board() {
+  return (
+    <div>Board</div>
+  )
+}
