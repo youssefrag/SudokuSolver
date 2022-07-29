bf5a338d336dbb2d41cabe2913ba4d865a98c914
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-29 11:21:14 -0400

    finish reset board functionality

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -53,7 +53,7 @@ function App() {
       element.remove()
     }
   }
-
+  
   const stringToArray = (string) => {
     const array = []
     for (let i = 0; i < string.length; i++) {
@@ -61,20 +61,7 @@ function App() {
     }
     return array
   }
-
-  const populateBoard = (board) => {
-    for (let i = 0; i < board.length; i++) {
-      const textFieldId = (i + 1).toString()
-      // console.log('field id:', textFieldId)
-      // console.log('value to be populated:', board[i])
-      document.getElementById(textFieldId).innerHTML = board[i]
-    }
-  }
-
-  const resetBoard = () => {
-    setRefreshState(refreshState + 1)
-  }
-
+  
   const handleSubmit = () => {
     let boardString = ''
     for (let i = 0; i < 81; i++) {
@@ -89,7 +76,6 @@ function App() {
       }
     }
     const boardObject = {}
-    // console.log(boardString)
     boardObject["puzzle"] = boardString
     axios.post(`http://localhost:6060/solve`, boardObject, {
       withCredentials: true,
@@ -98,14 +84,14 @@ function App() {
       console.log(result.data)
       if (result.data.solvable === false) {
         const messageContainer = document.querySelector('#message-container')
-
+        
         const message = document.createElement("h1")
-
+        
         message.setAttribute("id", "message")
         message.setAttribute("className", "message")
-
+        
         message.textContent = "Board is not solvable!"
-
+        
         if (!document.getElementById("message")) {
           messageContainer.prepend(message)
         }
@@ -113,14 +99,23 @@ function App() {
         const solutionString = result.data.solution
         const solutionArray = stringToArray(solutionString)
         setBoard(solutionArray)
-        // console.log(solutionArray)
-        // populateBoard(solutionArray)
       }
     }).catch((err) => {
       console.log(err.message)
     })
   }
-
+  
+  const handleReset = () => {
+    setBoard(emptyBoard)
+    eraseBoard()
+  }
+  
+  const eraseBoard = () => {
+    for (let i = 0; i < emptyBoard.length; i++){
+      const fieldIndex = (i + 1).toString()
+      document.getElementById(fieldIndex).value = null
+    }
+  }
   return (
     <div className={classes.root}>
       <div id="message-container" className={classes.msgContainer}>
@@ -142,7 +137,7 @@ function App() {
           Solve
         </Button>
         <Button
-          onClick={() => setRefreshState(refreshState + 1)}
+          onClick={handleReset}
           variant='contained'
           sx={{
             width: '90%',
